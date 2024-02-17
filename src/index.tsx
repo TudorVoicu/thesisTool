1c19af9f354058a8a7d064295d361c607f92233c
TudorVoicu <tudor.voicu99@gmail.com>
2024-02-17 19:51:05 +0100

    yes

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App.jsx'
+import './index.css'
+import {BrowserRouter} from "react-router-dom"
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+        <App />
+        </BrowserRouter>
+    </React.StrictMode>
+)
