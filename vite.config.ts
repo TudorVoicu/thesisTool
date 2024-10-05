82c9a96fd05f2f73748ad8079cc17865103e618e
TudorVoicu <tudor.voicu99@gmail.com>
2024-10-05 16:41:07 +0200

    help4...

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/thesisTool/',
   plugins: [react()],
-  base: '/thesisTool/'
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // Ensure that Rollup knows your input file
+    },
+  },
 });
